89309447af2a7b15436058d554df7d5e74f0355f
Mike Putnam <mike@theputnams.net>
2015-10-17 18:05:39 -0500

    Broken but checkpoint anyway.

--- mobile/src/main/java/theputnams/net/isitrecyclingweek/SearchActivity.java
+++ mobile/src/main/java/theputnams/net/isitrecyclingweek/SearchActivity.java
@@ -19,6 +19,7 @@ package theputnams.net.isitrecyclingweek;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.InputFilter;
 import android.text.SpannableString;
@@ -33,6 +34,7 @@ import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
@@ -52,7 +54,7 @@ public class SearchActivity extends Activity {
         }
     }
 
-    public void button_search_onclick(View view) {
+    public void button_search_onclick(View view) throws UnsupportedEncodingException {
 
         InputFilter inputFilter = new InputFilter() {
             @Override
@@ -88,44 +90,67 @@ public class SearchActivity extends Activity {
         input_street_address.setFilters(new InputFilter[] { inputFilter });
         String string_street_address = input_street_address.getText().toString();
 
-        String search_endpoint = null;
-        try {
-            search_endpoint = getString(R.string.api_uri) + "/search?q=" + URLEncoder.encode(string_street_address, "UTF-8");
-            Log.d("search_endpoint: ",search_endpoint);
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-
         String http_payload = null;
         try {
-            http_payload = new WebAsyncTask().execute(search_endpoint).get();
+            String foo = getString(R.string.api_uri) + "/api/implementations?addr=" + URLEncoder.encode(string_street_address, "UTF-8");
+
+            http_payload = new WebAsyncTask().execute(foo).get();
+
             if (http_payload != null) {
                 Log.d("http_payload: ", http_payload);
-
                 JSONArray json_array = new JSONArray(http_payload);
                 Log.d("json_array: ", String.valueOf(json_array));
 
                 int length = json_array.length();
-                ArrayList<String> result_arraylist = new ArrayList<>(length);
                 for (int i = 0; i < length; i++) {
-                    result_arraylist.add(json_array.getString(i));
+                    JSONObject obj = json_array.getJSONObject(i);
+                    if (obj.getString("contractName") == "upcoming-garbage-recycling-dates") {
+                        Log.d("implementationApiUrl",obj.getString("implementationApiUrl"));
+                    }
                 }
 
-                if (result_arraylist.size() < 1) {
+/*                if (length < 1) {
                     Toast.makeText(getApplicationContext(),"Sorry. Nothing found.", Toast.LENGTH_LONG).show();
                 } else {
-                    Intent intent = new Intent(this, SearchResultListActivity.class);
-                    intent.putStringArrayListExtra("result_arraylist",result_arraylist);
-                    startActivity(intent);
-                }
+                    //Let's go render the main activity!
+                    startActivity(new Intent(this, GarbageAndOrRecycleActivity.class));
+                }*/
 
             } else {
-                Toast.makeText(getApplicationContext(),"Sorry. Couldn't fetch data.", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(),"Sorry. Nothing found.", Toast.LENGTH_LONG).show();
             }
         } catch (JSONException | InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
 
+        //get back implementationApiUrl
+
+        /*
+        [
+          {
+            "contractName": "upcoming-garbage-recycling-dates",
+            "contractDescription": "Returns upcoming dates for garbage and recycling collection",
+            "implementationName": "appleton-wi-garbage",
+            "homepageUrl": "http://appletonapi.appspot.com",
+            "implementationApiUrl": "http://appletonapi.appspot.com/garbagecollection"
+          }
+        ]
+        */
+
+        //example: "implementationApiUrl": "http://appletonapi.appspot.com/garbagecollection"
+        //make call to "implementationApiUrl"; get back...
+
+        //Try saving the user's choice for subsequent runs of the app
+        try {
+            SharedPreferences preferences = getSharedPreferences("recyclingWeek", MODE_PRIVATE);
+            SharedPreferences.Editor edit = preferences.edit();
+
+            edit.putString("address", string_street_address);
+            edit.commit();
+
+        } catch (Exception e) {
+            Log.e("ERROR", "Failed to save address to preferences. " + e);
+        }
     }
 
     public static class SearchFragment extends Fragment {
