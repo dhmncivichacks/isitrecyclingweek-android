bb08531e2e88822091ca7bc3323128fba3dd855a
Jacob Kiser <jake.kiser@smartsocialmedia.com>
2015-10-25 15:06:04 -0500

    Adds some doc blocks and getters and setters

--- mobile/src/main/java/theputnams/net/isitrecyclingweek/restclients/model/CollectionEvent.java
+++ mobile/src/main/java/theputnams/net/isitrecyclingweek/restclients/model/CollectionEvent.java
@@ -8,14 +8,39 @@ public class CollectionEvent {
 
     @Expose
     private Date collectionDate;
+
     @Expose
     private String collectionType;
 
+    /**
+     *
+     * @return
+     */
     public Date getCollectionDate() {
         return collectionDate;
     }
 
+    /**
+     *
+     * @param collectionDate
+     */
     public void setCollectionDate(Date collectionDate) {
         this.collectionDate = collectionDate;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getCollectionType() {
+        return collectionType;
+    }
+
+    /**
+     *
+     * @param collectionType
+     */
+    public void setCollectionType(String collectionType) {
+        this.collectionType = collectionType;
     }
 }
